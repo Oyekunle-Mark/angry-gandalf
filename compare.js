c09bbb750c841d90b23ce66dfa28f6f370ac8f6e
oyekunle-mark <oyekunlemac@gmail.com>
2019-07-22 16:34:41 +0100

    Refactored to return a promise

--- compare.js
+++ compare.js
@@ -1,12 +1,20 @@
 const md5 = require('md5');
 
-module.exports = (originalPassword, hashedPassword) => {
-  const [rounds, salt, hash] = hashedPassword.split('$');
-  let result = originalPassword;
+module.exports = (originalPassword, hashedPassword) =>
+  new Promise((resolve, reject) => {
+    if (!originalPassword || !hashedPassword)
+      reject(
+        Error('Expects two inputs. Original password and hashed password.'),
+      );
 
-  for (let i = 0; i < 2 ** rounds; i++) {
-    result = md5(result + salt);
-  }
+    const [rounds, salt, hash] = hashedPassword.split('$');
+    let result = originalPassword;
 
-  return result === hash ? true : false;
-};
+    for (let i = 0; i < 2 ** rounds; i++) {
+      result = md5(result + salt);
+    }
+
+    if (result !== hash) resolve(false);
+
+    resolve(true);
+  });
